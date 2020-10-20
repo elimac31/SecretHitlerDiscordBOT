30fba8a1bd193354bb199741205ef7785d1a1afd
Eli MacDonald <epm9587@rit.edu>
2020-10-20 16:28:55 -0400

    added index and (hopefully) have a working test command

---
+++ index.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+"""
+This file is meant to handle most of the start up for the bot and (possibly) checking for commands
+"""
+
+
+def getToken():
+    try:
+        TOKEN = open("storedData/token.txt", "r")
+        return TOKEN.readline()
+    except:
+        return("ERROR, TOKEN NOT FOUND")
+
+
+client = commands.Bot(command_prefix='~')
+client.run(getToken())
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round (client.latency * 1000)}ms')
+
+
+
+
