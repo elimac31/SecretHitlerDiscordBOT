2c699f236be4cda116e020a5da5bdf9f14d7f024
Eli MacDonald <epm9587@rit.edu>
2020-10-21 16:25:19 -0400

    added functions and documentation to player class

--- player.py
+++ player.py
@@ -1,5 +1,8 @@
 import discord
-
+"""
+This class is for creating player objects
+it keeps track of who the player is, their role, thier party, and if they have been killed
+"""
 class player:
 
     def __init__(self, name, role, party):
@@ -8,15 +11,28 @@ class player:
         self.party = party
         self.isAlive = True
 
+    """
+    is called when this player is killed
+    returns their role to determine if they were hitler
+    """
     def isKilled(self):
         self.isAlive = False
         return self.role
 
+    """
+    returns this player's party
+    """
     def getParty(self):
         return self.party
 
+    """
+    returns this player's role
+    """
     def getRole(self):
         return self.role
 
+    """
+    Returns this player's name
+    """
     def getName(self):
         return self.name
