9bbb2ed28722ef28077388f16eb4fd7ec36c30fc
Eli MacDonald <epm9587@rit.edu>
2020-10-22 00:49:09 -0400

    created gameMain and stubbed both gameMain and board

---
+++ gameMain.py
@@ -0,0 +1,26 @@
+import discord
+import gameObjects
+
+class gameMain:
+
+    def __init__(self, players:[]):
+        pass
+
+    def roleSelection(self):
+        pass
+
+    def startUp(self):
+        pass
+
+    def election(self):
+        pass
+
+    def policySelection(self):
+        pass
+
+    def gameOver(self):
+        pass
+
+    """Should be last function as this will be the primary loop for the game and call most (if not) all others"""
+    def mainLoop(self):
+        pass
--- gameObjects/board.py
+++ gameObjects/board.py
@@ -1,1 +1,33 @@
 import discord
+from policyCard import policyCard
+from policyDeck import policyDeck
+
+
+class board:
+
+    def __init__(self, playerCount):
+        pass
+
+    def playPolicy(self):
+        pass
+
+    def failElection(self):
+        pass
+
+    def passElection(self):
+        pass
+
+    def checkFailedElections(self):
+        pass
+
+    def getFacistPolicies(self):
+        pass
+
+    def getLiberalPolicies(self):
+        pass
+
+    def printBoard(self):
+        pass
+
+    def isGameOver(self):
+        pass
